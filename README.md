1873c13103f63034e83a851925ab752305138912
LyraH736 <lyrah736@gmail.com>
2020-11-15 06:15:44 +0200

    Initial code and README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# era
-Easily Retargetable Assembler
+# Era
+
+An Easily Retargetable Assembler
+
+Designed for maximum compatability with different architectures, allowing the user to easily create a new target that works with all features of the assembler right out of the box.
+
+
+# Features
+
+* TODO - Comments
+* TODO - Constants
+* TODO - Labels
+* TODO - Support for binary, octal, hexadecimal, and negative decimal numbers
+* TODO - String support
+* TODO - Error reporting
+* TODO - Misspelling suggestions
+* TODO - Pseudo-instruction support
+* TODO - Macro support
+* TODO - Disassembler
+
+
+# Goals
+
+* Keep both the assembler code and target definition simple and readable
+* Allow every aspect of the assembler to be used on any target
+* Any target errors should be reported in an easily debugable way
---
+++ main.py
@@ -0,0 +1,42 @@
+import argparse
+import sys
+import assembler
+
+
+def main():
+    """Initial setup operation function"""
+    parser = argparse.ArgumentParser(
+        description='Era - an Easy Retargetable Assembler')
+    parser.add_argument('-t','--target',help='target')
+    parser.add_argument('-i','--input',help='input file')
+    parser.add_argument('-o','--output',help='output file')
+    parser.add_argument('-v','--verbose',help='Verbose output',
+        action='store_true')
+    args = parser.parse_args()
+    
+    # Set targets path and import set target or default(MIPS I)
+    sys.path.append('./targets')
+    
+    if args.target:
+        target = __import__(args.target)
+    else:
+        target = __import__('mips_i')
+    
+    # Import assembly file
+    with open(args.input,'r') as asmfile:
+        asmLines = asmfile.readlines()
+    
+    # Initialize 
+    asm_object = assembler(asmLines,target)
+    
+    # Assemble and convert to a binary object
+    memoryArray = bytes(asm_object.assemble(args.verbose))
+        
+    # Assembled file writer
+    outname = args.output if args.output else 'a.out'
+    with open(outname,'wb') as outfile:
+        outfile.write(memoryArray)
+
+
+if __name__ == '__main__':
+    main()
